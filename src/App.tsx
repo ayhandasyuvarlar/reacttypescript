74d1e00e91890e342d9d68ecfddc5f1b9426892c
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-10-17 19:01:50 +0300

    useReducer Strict Action Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Counter from './components/state/Counter'
-import User from './components/state/User'
+import { CounterTwo } from './components/state/CounterTwo'
 
 export default function App() {
   return <div>
--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -1,35 +1,36 @@
-import { useReducer } from "react";
+import { useReducer } from 'react'
 
-const initialState = { count: 0 };
+const initialState = { count: 0 }
 
 type ACTIONTYPE =
-  | { type: "increment"; payload: number }
-  | { type: "decrement"; payload: string };
+  | { type: 'increment'; payload: number }
+  | { type: 'decrement'; payload: string }
 
 function reducer(state: typeof initialState, action: ACTIONTYPE) {
   switch (action.type) {
-    case "increment":
-      return { count: state.count + action.payload };
-    case "decrement":
-      return { count: state.count - Number(action.payload) };
+    case 'increment':
+      return { count: state.count + action.payload }
+    case 'decrement':
+      return { count: state.count - Number(action.payload) }
+
     default:
-      throw new Error();
+      throw new Error()
   }
 }
 
 function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
       Count: {state.count}
-      <button onClick={() => dispatch({ type: "decrement", payload: "5" })}>
+      <button onClick={() => dispatch({ type: 'decrement', payload: '5' })}>
         -
       </button>
-      <button onClick={() => dispatch({ type: "increment", payload: 5 })}>
+      <button onClick={() => dispatch({ type: 'increment', payload: 5 })}>
         +
       </button>
     </>
-  );
+  )
 }
 
 export default Counter
