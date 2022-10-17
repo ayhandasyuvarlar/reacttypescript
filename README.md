fbadf25919bee39d1879330616790ae345faf202
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-10-17 15:53:26 +0300

    useState Type Assertion

--- README.md
+++ README.md
@@ -384,3 +384,34 @@ export default function User() {
   )
 }
 ```
+
+
+### useState Type Assertion
+
+```js 
+// before
+const [user, setUser] = (useState < AuthUser) | (null > null)   // <----
+// after 
+import { useState } from 'react'
+import { AuthUser } from './User.types'
+
+export default function User() {
+  const [user, setUser] = useState<AuthUser>({} as AuthUser)
+  const handleLogin = () => {
+    setUser({
+      name: 'Ayhan',
+      email: 'ayhandasyuvarlar3@gmail.com',
+    })
+  }
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <div>
+        user name is {user.name}
+      </div>
+      <div>user email is {user.email}</div>
+    </div>
+  )
+}
+
+```
--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -2,25 +2,20 @@ import { useState } from 'react'
 import { AuthUser } from './User.types'
 
 export default function User() {
-  const [user, setUser] = useState<AuthUser | null>(null)
+  const [user, setUser] = useState<AuthUser>({} as AuthUser)
   const handleLogin = () => {
     setUser({
       name: 'Ayhan',
       email: 'ayhandasyuvarlar3@gmail.com',
     })
   }
-  const handleLogout = () => {
-    setUser(null)
-  }
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
       <div>
-        {user === null
-          ? 'Please click login'
-          : ` user Name :${user?.name}  user Email : ${user?.email}`}
+        user name is {user.name}
       </div>
+      <div>user email is {user.email}</div>
     </div>
   )
 }
